a653e54a2567076dfa232ef75917108856109042
eunkang-dyphi <eunkang@dyphi.com>
2024-03-20 17:02:58 +0900

    :sparkles: Init: 환경셋팅작업 (prisma 추가 & docker 추가)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MainModules } from 'src/modules';
 
 @Module({
-  imports: [],
+  imports: [...MainModules],
   controllers: [AppController],
   providers: [AppService],
 })
