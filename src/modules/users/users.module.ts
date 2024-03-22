8fc87a1c35bbfc3e5b34cae8cf35e5c01b8f7a1a
loveAlakazam <dmsrkd1216@gmail.com>
2024-03-23 00:43:16 +0900

    :bug: Fix: 유저모듈에 DB프로바이더 추가

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,10 +5,19 @@ import { UsersController } from './controllers/users.controller';
 import { PrismaModule } from '../core/database/prisma/prisma.module';
 import { UserProfilesController } from './controllers/user-profiles.controller';
 import { UserProfileService } from './services/user-profiles.service';
+import { UsersRepository } from './repositories/users.interface';
+import { UsersRepositoryImpl } from './repositories/users.repository';
 
 @Module({
   imports: [PrismaModule, RouterModule.register([{ path: 'api/users' }])],
-  providers: [UsersService, UserProfileService],
+  providers: [
+    UsersService,
+    UserProfileService,
+    {
+      provide: UsersRepository,
+      useClass: UsersRepositoryImpl,
+    },
+  ],
   controllers: [UsersController, UserProfilesController],
   exports: [UsersService],
 })
