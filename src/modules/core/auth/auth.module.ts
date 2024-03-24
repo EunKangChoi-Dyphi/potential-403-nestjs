a8aa300428815520c8edee0c3a907064b2c95c2a
loveAlakazam <dmsrkd1216@gmail.com>
2024-03-25 01:51:23 +0900

    :package: Chore: 패키지설치 & auth모듈 및 소셜로그인파트 초안작업진행중

--- src/modules/core/auth/auth.module.ts
+++ src/modules/core/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { HttpModule } from '@nestjs/axios';
+import { CustomConfigModule } from '../config/custom-config.module';
 
-@Module({})
+@Module({
+  imports: [HttpModule, CustomConfigModule],
+  providers: [AuthService],
+  exports: [AuthService],
+})
 export class AuthModule {}
--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -11,20 +11,20 @@ import {
 import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dtos/req/create-user.dto';
 import { UpdateUserRequestBodyDto } from '../dtos/req/create-user-request-body.dto';
+import { SignInUser } from 'src/decorators/sign-in-user.decorator';
+import { UserEntity } from '../entities/user.entity';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  // 회원가입 (일반)
-  @Post()
-  async createUser(@Body() body: CreateUserDto) {
-    return await this.usersService.createUser(body);
-  }
 
-  // 회원정보 조회
-  @Get(':id')
-  async getOneUser(@Param('id', ParseIntPipe) id: number) {
-    return await this.usersService.getOneUser(id);
+  // 로그인한 회원정보 조회
+  @Get()
+  async getOneUser(
+    @Param('id', ParseIntPipe) id: number,
+    @SignInUser() user: UserEntity, // login required
+  ) {
+    return await this.usersService.getOneUser(user.id);
   }
 
   // 회원정보 수정
@@ -39,13 +39,14 @@ export class UsersController {
 
   // 회원탈퇴
   @Delete(':id')
-  async deleteUser(@Param('id', ParseIntPipe) id: number) {
+  async withdrawUser(@Param('id', ParseIntPipe) id: number) {
     return await this.usersService.deleteUser(id);
   }
 
+  // 소셜로그인겸 & 회원가입
   // 소셜로그인 - 카카오
-  @Post('login/kakao')
-  async socialLoginKakao() {}
   // 소셜로그인 - 구글
   // 소셜로그인 - 애플
+  @Post('sign-in')
+  async signInWithSocialOauth() {}
 }
