f1edc039857ff92fe03a7d10ef55d84376c37136
DongSeong Seo <sds901234@naver.com>
2024-03-29 22:41:35 +0900

    :white_check_mark: Test: 파일 업로드 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { RedisService } from 'src/modules/core/redis/redis.service';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { writeFileSync } from 'fs';
 
 @Controller()
 export class AppController {
@@ -19,5 +29,27 @@ export class AppController {
     const { key, value } = body;
     await this.redisService.set(key, value, 60 * 60 * 24);
     return 'OK';
+  }
+
+  @Post('file-upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async testFileUpload(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: any,
+  ) {
+    console.log(file);
+    writeFileSync('test.jpg', file.buffer);
+
+    console.log(body);
+  }
+
+  @Post('file-upload2')
+  @UseInterceptors(FilesInterceptor('files'))
+  async testFileUpload2(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body: any,
+  ) {
+    console.log(files);
+    console.log(body);
   }
 }
