ee3c25cc56b9dc85da5110851a3b0a555ce20bcb
loveAlakazam <dmsrkd1216@gmail.com>
2024-03-30 16:22:02 +0900

    :recycle: Refactor: 유저정보수정 비즈니스 로직 수정 & repository단 삭제

--- src/modules/users/controllers/user-profiles.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UserProfileService } from '../services/user-profiles.service';
-
-@Controller(':id/profile')
-export class UserProfilesController {
-  constructor(private readonly userProfileService: UserProfileService) {}
-}
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,76 +1,87 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { UsersRepository } from "../repositories/users.interface";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateUserDto } from "../dtos/req/create-user.dto";
 import { UpdateUserDto } from "../dtos/req/update-user.dto";
 import { UpdateUserRequestBodyDto } from "../dtos/req/update-user-request-body.dto";
 import { AwsS3Service } from "src/modules/core/aws-s3/aws-s3.service";
+import { PrismaService } from "src/modules/core/database/prisma/prisma.service";
 
 @Injectable()
 export class UsersService {
   constructor(
-    @Inject(UsersRepository) private readonly userRepository: UsersRepository,
+    private readonly prismaService: PrismaService,
     private readonly awsS3Service: AwsS3Service
   ) {}
 
-  /**
-   * @param userId
-   */
   async getOneUser(userId: number) {
-    const user = await this.userRepository.findOne({ id: userId });
+    const user = this.prismaService.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
     return user;
   }
 
-  /**
-   * @param email
-   * @param name
-   */
-  async createUser(dto: CreateUserDto) {
-    const newUser = await this.userRepository.createUser(dto);
+  createUser(dto: CreateUserDto) {
+    const newUser = this.prismaService.user.create({
+      data: {
+        ...dto,
+      },
+    });
     return newUser;
   }
 
-  /**
-   * @param userId
-   * @param name
-   */
   async updateUser(dto: UpdateUserRequestBodyDto) {
     const { id, name, intro, profileImageFile } = dto;
 
-    const user = await this.userRepository.findOne({ id: id });
-    // DB에서 유저프로필이미지 가 저장되어있는지 확인
-    // if(user.profileImageURL) {
-    //   // s3에 있는 기존의 이미지를 지운다.
-    //   await this.awsS3Service.deleteImageFromS3Bucket({
-    //     Key: `${id}/profiles/${}`
-    //   })
-    //   // s3에 새로운 프로필이미지로 업데이트한다.
-    // }
+    this.prismaService.$transaction(async (transaction) => {
+      // 1. 유저정보 유무 확인
+      const user = await transaction.user.findFirst({ where: { id: id } });
+      if (!user) {
+        throw new NotFoundException("존재하지 않은 회원입니다.");
+      }
 
-    // if(profileImageFile) {
+      // 2. 프로필 이미지 URL 셋팅
+      const oldProfileImageS3URL = user.profileImageURL;
+      const newProfileImageS3URL = profileImageFile
+        ? this.awsS3Service.publishS3URL(`profiles/${id}/${profileImageFile.originalname}`)
+        : oldProfileImageS3URL;
 
-    //   // s3에 이미지를 업데이트한다.
-    //   const profileImageURL = await this.awsS3Service.uploadImageToS3Bucket({
-    //     Key: `${id}/profiles/${}`,
-    //     Body:,
-    //     ContentType: dto.,
-    //   });
-    // }
+      // 3. 유저정보를 업데이트한다
+      const updatedUser = await transaction.user.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name: name ?? user.name,
+          intro: intro ?? user.intro,
+          profileImageURL: newProfileImageS3URL,
+        },
+      });
 
-    // 유저정보 업데이트
-    const updatedUser = await this.userRepository.updateUser({
-      id: id,
-      name: name,
-      intro: intro,
-      // profileImageURL: profileImageURL,
+      // 4. DB 트랜잭션처리 완료후, 프로필이미지 파일이 존재할때, S3 업데이트
+      if (profileImageFile) {
+        // 4-1. DB에서 유저프로필이미지가 존재하는지 확인 확인
+        if (user.profileImageURL) {
+          // 기존의 이미지와 디렉토리를 지운다
+          await this.awsS3Service.deleteImageFromS3Bucket({
+            Key: `profiles/${id}`,
+          });
+
+          // 4-2. s3에 새로운 프로필이미지로 업데이트한다.
+          await this.awsS3Service.uploadImageToS3Bucket({
+            Key: `profiles/${id}/${profileImageFile.originalname}`,
+          });
+        }
+      }
+
+      return updatedUser;
     });
-    return updatedUser;
   }
 
-  /**
-   * @param userId
-   */
-  async deleteUser(userId: number) {
-    const deletedUser = await this.userRepository.deleteUser(userId);
+  deleteUser(userId: number) {
+    const deletedUser = this.prismaService.user.delete({
+      where: { id: userId },
+    });
     return deletedUser;
   }
 }
