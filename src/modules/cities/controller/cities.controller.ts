424aab74d49d085118945e6faec8c0629b75715f
loveAlakazam <dmsrkd1216@gmail.com>
2024-03-30 22:05:02 +0900

    :memo: Update: 도시/국가 조회회API만 보이도록 수정

--- src/modules/cities/controller/cities.controller.ts
+++ src/modules/cities/controller/cities.controller.ts
@@ -14,14 +14,14 @@ import { CitiesService } from "../service/cities.service";
 import { CreateCityDto } from "../dto/create-city.dto";
 import { UpdateCityDto } from "../dto/update-city.dto";
 import { SearchCityDto } from "../dto/search-city.dto";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiExcludeEndpoint, ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @ApiTags("City")
 @Controller()
 export class CitiesController {
   constructor(private readonly citiesService: CitiesService) {}
 
-  @ApiOperation({ summary: "도시 등록" })
+  @ApiExcludeEndpoint()
   @Post()
   create(@Body() createCityDto: CreateCityDto) {
     return this.citiesService.create(createCityDto);
@@ -33,11 +33,13 @@ export class CitiesController {
     return this.citiesService.findAll(query);
   }
 
+  @ApiExcludeEndpoint()
   @Put(":id")
   update(@Param("id", ParseIntPipe) id: string, @Body() updateCityDto: UpdateCityDto) {
     return this.citiesService.update(+id, updateCityDto);
   }
 
+  @ApiExcludeEndpoint()
   @Delete(":id")
   delete(@Param("id") id: string) {
     return this.citiesService.delete(+id);
--- src/modules/conuntries/controller/countries.controller.ts
+++ src/modules/conuntries/controller/countries.controller.ts
@@ -13,26 +13,34 @@ import { CountriesService } from "../service/countries.service";
 import { CreateCountryDto } from "src/modules/conuntries/dtos/create-country.dto";
 import { UpdateCountryDto } from "src/modules/conuntries/dtos/update-country.dto";
 import { SearchCountryDto } from "../dtos/search-country.dto";
+import { ApiExcludeEndpoint, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Country")
 @Controller()
 export class CountriesController {
   constructor(private readonly countriesService: CountriesService) {}
 
+  @ApiExcludeEndpoint()
   @Post()
   create(@Body() dto: CreateCountryDto) {
     return this.countriesService.create(dto);
   }
 
+  @ApiOperation({
+    summary: "국가 검색",
+  })
   @Get()
   findAll(@Query() dto: SearchCountryDto) {
     return this.countriesService.findAll(dto);
   }
 
+  @ApiExcludeEndpoint()
   @Put(":code")
   update(@Param("code") code: string, @Body() dto: UpdateCountryDto) {
     return this.countriesService.update(code, dto);
   }
 
+  @ApiExcludeEndpoint()
   @Delete(":code")
   delete(@Param("code") code: string) {
     return this.countriesService.delete(code);
